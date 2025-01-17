5923ec8678c377c62b3d0ad511347cb3d3a81897
Graham Hall <graham@ghall.dev>
2025-01-17 08:26:05 -0500

    fix settings view when 'none' is selected

--- AutoDock/SettingsView.swift
+++ AutoDock/SettingsView.swift
@@ -1,19 +1,22 @@
-import SwiftUI
 import LaunchAtLogin
+import SwiftUI
 
 struct SettingsView: View {
 	@AppStorage("minWidthToShowDock") private var minWidthToShowDock = 0.0
 	@EnvironmentObject var displayManager: DisplayManager
-	
+
 	var body: some View {
 		Form {
 			Picker(selection: $minWidthToShowDock) {
 				ForEach(displayManager.currentDisplays, id: \.self) { display in
 					let displayName = display.localizedName
-					
+
 					Text("\(createLabel(display.frame.width)) - \(displayName)").tag(Double(display.frame.width))
 				}
-				if displayManager.currentDisplays.count(where: { $0.frame.width == minWidthToShowDock }) == 0 {
+				if
+					displayManager.currentDisplays.count(where: { $0.frame.width == minWidthToShowDock }) == 0
+					&& minWidthToShowDock != 0.0
+				{
 					Divider()
 					Text(createLabel(minWidthToShowDock)).tag(minWidthToShowDock)
 				}
@@ -27,7 +30,7 @@ struct SettingsView: View {
 		.formStyle(.grouped)
 		.frame(width: 400, height: 120)
 	}
-	
+
 	private func createLabel(_ value: Double) -> String {
 		return "\(value)pts"
 	}
