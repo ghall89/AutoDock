a2b11b88017e31e5ad5afdc1ed00e5467ad7deb2
Graham Hall <graham@ghall.dev>
2025-01-27 12:40:09 -0500

    use AppleScript for dock visibility

--- AutoDock/Utilities.swift
+++ AutoDock/Utilities.swift
@@ -1,26 +1,24 @@
+import AppleScriptObjC
 import Foundation
 
 func toggleDockVisibility(hidden: Bool) {
 	// don't bother updating dock autohide setting if it matches the new value
-	if let currentPref = getDockAutohidePref() {
-		let writeTask = Process()
-		writeTask.launchPath = "/usr/bin/defaults"
-		writeTask.arguments = ["write", "com.apple.dock", "autohide", "-bool", hidden.description]
+	if getDockAutohidePref() != hidden {
+		let script = """
+			tell application "System Events"
+				set autohide of dock preferences to \(hidden.description)
+			end tell
+			"""
 		
-		let killDockTask = Process()
-		killDockTask.launchPath = "/usr/bin/killall"
-		killDockTask.arguments = ["Dock"]
-		
-		do {
-			try writeTask.run()
-			writeTask.waitUntilExit()
+		if let appleScript = NSAppleScript(source: script) {
+			var error: NSDictionary?
 			
-			try killDockTask.run()
-			killDockTask.waitUntilExit()
-			
-			print(hidden ? "Dock is now hidden." : "Dock is now visible.")
-		} catch {
-			print("Failed to update Dock settings: \(error)")
+			appleScript.executeAndReturnError(&error)
+			if let error = error {
+				print(error)
+			}
+		} else {
+			print("Failed to create AppleScript.")
 		}
 	}
 }
@@ -29,24 +27,24 @@ func getDockAutohidePref() -> Bool? {
 	let readTask = Process()
 	readTask.launchPath = "/usr/bin/defaults"
 	readTask.arguments = ["read", "com.apple.dock", "autohide"]
-	
+
 	let pipe = Pipe()
 	readTask.standardOutput = pipe
-	
+
 	do {
 		try readTask.run()
 		readTask.waitUntilExit()
-		
+
 		let data = pipe.fileHandleForReading.readDataToEndOfFile()
 		if let output = String(data: data, encoding: .utf8) {
 			let trimmedOutput = output.trimmingCharacters(in: .whitespacesAndNewlines)
 			let dockIsCurrentlyHidden = trimmedOutput == "1"
-			
+
 			return dockIsCurrentlyHidden
 		}
 	} catch {
 		print("Failed to read Dock settings: \(error)")
 	}
-	
+
 	return nil
 }
