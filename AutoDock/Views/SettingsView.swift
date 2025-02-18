c63c8ec9ac005eba0807ac7bdd9810f6fcb46d52
Graham Hall <graham@ghall.dev>
2025-02-17 21:26:23 -0500

    display manager fixes

--- AutoDock/Views/SettingsView.swift
+++ AutoDock/Views/SettingsView.swift
@@ -2,47 +2,36 @@ import LaunchAtLogin
 import SwiftUI
 
 struct SettingsView: View {
-
+	@AppStorage("minWidthToShowDock") private var minWidthToShowDock = 0.0
 	@EnvironmentObject var displayManager: DisplayManager
-
+	
 	var body: some View {
 		Form {
-			Section {
-				Picker(selection: $displayManager.minWidthToShowDock) {
-					ForEach(displayManager.connectedDisplays, id: \.self) { display in
-						let displayName = display.localizedName
-						
-						Text("\(getResolutionString(display.frame.size)) - \(displayName)").tag(Double(display.frame.width))
-					}
-					if !displayManager.displayHistory.isEmpty {
-						Divider()
-						ForEach(displayManager.displayHistory) { display in
-							let displayInfo = display.displayInfo
-							let displayName = displayInfo.localizedName
-							if !displayManager.connectedDisplays.contains(where: { isSameDisplay(displayOne: $0, displayTwo: displayInfo) }) {
-								Text("\(getResolutionString(displayInfo.frame.size)) - \(displayName)").tag(Double(displayInfo.frame.width))
-							}
-						}
-					}
-					Divider()
-					Text("None").tag(0.0)
-				} label: {
-					Text("Minimum Resolution")
+			Picker(selection: $minWidthToShowDock) {
+				ForEach(displayManager.connectedDisplays, id: \.self) { display in
+					let displayName = display.localizedName
+					
+					Text("\(createLabel(display.frame.width)) - \(displayName)").tag(Double(display.frame.width))
 				}
-				.help("The minimum display resolution for the dock to be visible.")
-				Toggle("Only Apply to Primary Display", isOn: $displayManager.onlyOnPrimaryDisplay)
-					.help("Only show/hide dock on your primary display (the display where your dock is located).")
+				if
+					displayManager.connectedDisplays.count(where: { $0.frame.width == minWidthToShowDock }) == 0
+						&& minWidthToShowDock != 0.0
+				{
+					Divider()
+					Text(createLabel(minWidthToShowDock)).tag(minWidthToShowDock)
+				}
+				Divider()
+				Text("None").tag(0.0)
+			} label: {
+				Text("Minimum Width to Show Dock")
 			}
-			Section {
-				LaunchAtLogin.Toggle()
-					.help("Automatically launch 'AutoDock' at startup.")
-			}
+			LaunchAtLogin.Toggle()
 		}
 		.formStyle(.grouped)
-		.frame(width: 400, height: 170)
+		.frame(width: 400, height: 120)
 	}
-
-	private func getResolutionString(_ size: CGSize) -> String {
-		return "\(size.width.description.replacingOccurrences(of: ".0", with: "")) x \(size.height.description.replacingOccurrences(of: ".0", with: ""))"
+	
+	private func createLabel(_ value: Double) -> String {
+		return "\(Int(value))pts"
 	}
 }
