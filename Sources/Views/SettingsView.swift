502c2b459023abfcdc4bf0884f4f887d0f5f94a2
Graham Hall <graham@ghall.dev>
2026-01-18 12:22:05 -0500

    add descriptors to settings view

--- Sources/Views/SettingsView.swift
+++ Sources/Views/SettingsView.swift
@@ -27,13 +27,22 @@ struct SettingsView: View {
 				} label: {
 					Text("Minimum screen resolution")
 				}
+				Text(
+					"Minimum resolution to turn off autohide. When you have a display connected at this resolution or higher, the Dock will be visible.",
+				)
+				.font(.footnote)
 			}
 			Section {
-				Toggle("Also toggle Menu Bar", isOn: $alsoToggleMenubar)
+				Toggle("Autohide Menu Bar", isOn: $alsoToggleMenubar)
+				Text(
+					"Toggle the Menu Bar's autohide setting with the Dock.",
+				)
+				.font(.footnote)
 			}
 			Section {
 				LaunchAtLogin.Toggle()
-
+			}
+			Section {
 				VStack(alignment: .leading) {
 					HStack {
 						Text("Permission to automate system events")
@@ -42,18 +51,18 @@ struct SettingsView: View {
 							.foregroundStyle(
 								displayManager.systemEventsPermitted ? Color.green : Color.red)
 					}
-					Text(
-						"AutoDock requires permission to automate system events in order to change the settings for your Dock.",
-					)
-					.font(.footnote)
 				}
+				Text(
+					"AutoDock requires permission to automate system events in order to change the settings for your Dock.",
+				)
+				.font(.footnote)
 				if !displayManager.systemEventsPermitted {
 					Button("Check System Privacy Settings", action: openPrivacySecuritySettings)
 				}
 			}
 		}
 		.formStyle(.grouped)
-		.frame(width: 400, height: 240)
+		.frame(width: 400, height: 350)
 	}
 
 	private func createLabel(_ value: CGRect) -> String {
